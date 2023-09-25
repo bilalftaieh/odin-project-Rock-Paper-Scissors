6503300126d01626b9131f6c0d44787921cd42eb
bilalftaieh <ftaiehbilal@gmail.com>
2023-09-25 12:22:06 +0300

    Transforming Rock Paper Scissors Browser Console game to a UI Game with HTM,CSS,JavaScript.
    I modified the old blank index.html to have a UI with
    new features and a great styling in styles.css,
    the game logic in script.js was modified so it can
    work with the new game UI and perform the necessary
    DOM manipulation to keep the player updated with
    the new informations of the game.

--- README.md
+++ README.md
@@ -1,12 +1,22 @@
 # odin-project-Rock-Paper-Scissors
 A rock paper scissors game project from the Odin Project.
 
-The game at this moment is played with any browser console.
+The game has a User Friendly UI and includes great features that
+includes Important Informations(Player Score,Computer Score,Round Number,etc...)
+and three game buttons(Rock,Paper,Scissors)
 
-The game is played at 5 rounds and each round win a point will be attributed to the winner.
+The game is played at unlimited rounds and each round win a point will be attributed to the winner.
 
-The one with the highest points once the 5 rounds is finished will win the game.
+The one with the highest points once the score reaches 5 points first will win the game.
 
-the user will play against his oppenent which is the computer.
+The user will play against his oppenent which is the computer.
 
-The round will not continue to the next one if there is a TIE! or a Validation problem with user input.
+The round will continue to the next one if there is a WIN,LOSE,and TIE!
+
+The game will start first time when the user chooses his first choice
+
+The game can be started again through the Start Again Button which will
+appear once the game finishes
+
+Note : This is the updated UI version of Odin Project Rock Paper Scissors
+
--- script.js
+++ script.js
@@ -1,90 +1,166 @@
+// Initialize game variables
+let gameCounter = 0;
+let playerScore = 0;
+let computerScore = 0;
+let selectedChoice = "";
+const gameButtons = document.querySelectorAll('.game-button');
 
-// function that returns the computer choice using Math.Random()
-let getComputerChoice = ()=>{
-    let randomNumber = Math.floor(Math.random()*3);
-    return ["rock","paper","scissors"][randomNumber];
+// Function to update player and computer scores in the DOM
+function setScoreDiv(playerScore, computerScore) {
+    let playerScoreSpan = document.querySelector('#player-score');
+    let computerScoreSpan = document.querySelector('#computer-score');
+    playerScoreSpan.innerHTML = playerScore;
+    computerScoreSpan.innerHTML = computerScore;
 }
 
-// function that returns the user choice lower cased for case insensitivity
-let getUserChoice = (choice)=> choice.toLowerCase();
+// Function to update the round counter in the DOM
+let setRoundCounterDiv = (roundNumber) => {
+    let roundNumberSpan = document.querySelector('#round');
+    roundNumberSpan.innerHTML = roundNumber;
+}
 
-// function that determines the result of the single round and return it
+// Function to update the result text in the DOM
+let setResultDiv = (winnerText) => {
+    let resultParagraph = document.querySelector("#result-text");
+    resultParagraph.innerHTML = winnerText;
+    resultParagraph.hidden = false;
+}
+
+let setGameButtonDisabled = (disable)=>{
+    gameButtons.forEach((button)=>{
+        if(disable){
+            button.disabled = true;
+        }
+        else{
+            button.disabled = false;
+        }
+    });
+}
+
+// Function to reset game state after finishing the game
+let setAllDivAfterGameFinishes = () => {
+    let startAgainBtn = document.querySelector("#start-again-button");
+    let computerChoiceParagraph = document.querySelector("#computer-choice");
+    
+    // Make game buttons disabled
+    setGameButtonDisabled(true);
+
+    // Show the "Start Again" button
+    startAgainBtn.hidden = false;
+    
+    // Add event listener to the "Start Again" button
+    startAgainBtn.addEventListener('click', () => {
+        // Reset game variables
+        playerScore = 0;
+        computerScore = 0;
+        gameCounter = 1; // Reset the gameCounter to 1
+        selectedChoice = "";
+        
+        // Update the round counter, player score, and result text
+        setRoundCounterDiv(gameCounter);
+        setScoreDiv(playerScore, computerScore);
+        setResultDiv("Not Determined");
+        
+        // Reset computer choice display
+        computerChoiceParagraph.textContent = "Not Chosen";
+        
+        //undisable game buttons
+        setGameButtonDisabled(false);
+
+        // Hide the "Start Again" button again
+        startAgainBtn.hidden = true;
+        
+    });
+}
+
+// Function that returns the computer choice using Math.Random()
+let getComputerChoice = (min, max) => {
+    let randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
+    return ["rock", "paper", "scissors"][randomNumber - 1];
+}
+
+// Function that returns the user choice in lowercase for case insensitivity
+let getUserChoice = (choice) => choice.toLowerCase();
+
+// Function that determines the result of a single round and returns it
 function playRound(playerSelection, computerSelection) {
-    if(playerSelection=="rock"){
-        if(computerSelection=="paper"){
+    if (playerSelection == "rock") {
+        if (computerSelection == "paper") {
             return "You LOSE! Paper beats Rock";
-        }
-        else if(computerSelection=="scissors"){
+        } else if (computerSelection == "scissors") {
             return "You WIN! Rock beats Scissors";
-        }
-        else{
+        } else {
             return "TIE!";
         }
-    }
-
-    else if(playerSelection=="paper"){
-        if(computerSelection=="scissors"){
+    } else if (playerSelection == "paper") {
+        if (computerSelection == "scissors") {
             return "You LOSE! Scissors Beats Paper ";
-        }
-        else if(computerSelection=="rock"){
+        } else if (computerSelection == "rock") {
             return "You WIN! Paper Beats Rock";
-        }
-        else{
+        } else {
             return "TIE!";
         }
-    }
-
-    else if(playerSelection=="scissors"){
-        if(computerSelection=="rock"){
+    } else if (playerSelection == "scissors") {
+        if (computerSelection == "rock") {
             return "You LOSE! Rock beats Scissors";
-        }
-        else if(computerSelection=="paper"){
+        } else if (computerSelection == "paper") {
             return "You WIN! Scissors beats Paper";
-        }
-        else{
+        } else {
             return "TIE!";
         }
     }
-
-  }
-
-  // function that begins the game for 5 rounds
-  function game(){
-    let gameCounter = 1;
-    let playerScore = 0;
-    let computerScore = 0;
-    while(true){
-        console.log("Round " + gameCounter);
-        let playerChoice = getUserChoice(prompt("Please Enter your choice? Rock/Paper/Scissors"));
-        let computerChoice = getComputerChoice();
-        // Validaton check if user inputs choices other than "rock","paper","scissors"
-        if(!["rock","paper","scissors"].includes(playerChoice)){
-            alert("Please enter a valid choice");
-            continue;
-        }
-        else{
-            let result = playRound(playerChoice,computerChoice);
-            alert("Computer Choice is " + computerChoice + ", " + result);
-            if(result.includes("WIN!")){
-                playerScore++;
-            }
-            else if(result.includes("LOSE!")){
-                computerScore++;
-            }
-            else if(result.includes("TIE!")){
-                continue;
-            }
-        }
-        gameCounter++;
-        console.log("Player Score : " + playerScore);
-        console.log("Computer Score : " + computerScore);
-        if(gameCounter == 6){
-            break;
-        }
-    }
-    alert((playerScore>computerScore)?"Player WINS!,Score : " + playerScore:"COMPUTER WINS,Score : " + 
-    computerScore);
 }
 
-game();
+// Function that begins the game for 5 rounds
+function game() {
+    let computerChoiceParagraph = document.querySelector("#computer-choice");
+    
+    // Iterate through each button and add a click listener
+    gameButtons.forEach((button) => {
+        button.addEventListener('click', () => {
+            // Increment the game counter
+            gameCounter++;
+            
+            // Update the round counter in the DOM
+            setRoundCounterDiv(gameCounter);
+            
+            // Get the selected choice
+            selectedChoice = button.innerHTML;
+            
+            // Get the user's choice and computer's choice
+            let playerChoice = getUserChoice(selectedChoice);
+            let computerChoice = getComputerChoice(1, 3);
+            
+            // Display the computer's choice in the DOM
+            computerChoiceParagraph.innerHTML = computerChoice;
+            
+            // Determine the result of the round
+            let result = playRound(playerChoice, computerChoice);
+            
+            // Update the result text, player score, and computer score in the DOM
+            if (result.includes("WIN!")) {
+                setResultDiv("YOU WIN THIS ROUND!");
+                playerScore++;
+            } else if (result.includes("LOSE!")) {
+                setResultDiv("YOU LOSE THIS ROUND!");
+                computerScore++;
+            } else {
+                setResultDiv("YOU TIE THIS ROUND!");
+            }
+            
+            setScoreDiv(playerScore, computerScore);
+            
+            // Check if the player or computer has won the game
+            if (playerScore == 5) {
+                setResultDiv("YOU WON THE GAME!");
+                setAllDivAfterGameFinishes();
+            } else if (computerScore == 5) {
+                setResultDiv("YOU LOST THE GAME!");
+                setAllDivAfterGameFinishes();
+            }
+        });
+    });
+}
 
+// Start the game
+game();
